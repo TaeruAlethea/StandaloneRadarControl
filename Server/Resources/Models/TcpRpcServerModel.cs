f81733cfcdb513d66399921941f9e8b5a6b9155c
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-02-28 14:25:26 -0500

    Create TcpRpcServerModel.cs

---
+++ Server/Resources/Models/TcpRpcServerModel.cs
@@ -0,0 +1,59 @@
+﻿using Server.Resources.Interfaces;
+
+namespace Server.Resources.Models;
+
+public class TcpRpcServerModel : ModelBase, IServerModel
+{
+	private int clientCount;
+	public int ClientCount
+	{
+		get => clientCount;
+		set
+		{
+			clientCount = value; 
+			OnPropertyChanged();
+		}
+	}
+	
+	private float pingInterval;
+	public float PingInterval	{
+		get => pingInterval;
+		set
+		{
+			pingInterval = value; 
+			OnPropertyChanged();
+		}
+	}
+	
+	private int updatesPerSecond;
+	public int UpdatesPerSecond	{
+		get => updatesPerSecond;
+		set
+		{
+			updatesPerSecond = value; 
+			OnPropertyChanged();
+		}
+	}
+	
+	private int activePort;
+	public int ActivePort	{
+		get => activePort;
+		set
+		{
+			activePort = value; 
+			OnPropertyChanged();
+		}
+	}
+	
+	
+	
+	public bool StartServer()
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool StopServer()
+	{
+		throw new NotImplementedException();
+	}
+}
