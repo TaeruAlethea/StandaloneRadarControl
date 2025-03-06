30e293cfd0b0797956eb1dbdf24ab9ae32cfcbd1
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-03-06 15:33:10 -0500

    Set up a Common Models Project

---
+++ Common/Models/Unit.cs
@@ -0,0 +1,17 @@
+﻿namespace Common.Models;
+
+public class Unit
+{
+	public uint Id { get; init; }
+	public string Name { get; init; }
+	public string Player { get; init; }
+	public string Callsign { get; init; }
+	public string GroupName { get; init; }
+	public int Coalition { get; init; }
+	public string Type { get; init; }
+	public Position Position { get; init; }
+	public double Altitude { get; init; }
+	public double Heading { get; init; }
+	public double Speed { get; init; }
+	public bool Deleted { get; init; }
+}
